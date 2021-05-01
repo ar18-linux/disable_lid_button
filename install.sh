f64b5a3be324a3c1f05fec6a03bccba73c5867d3
nulysses <adr.zivil@gmail.com>
2021-05-01 11:31:10 +0200

    1

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ rm -rf "${install_dir}/${module_name}"
 cp -rf "${script_dir}/${module_name}" "${install_dir}/${module_name}"
 chmod +x "${install_dir}/${module_name}/"* -R
 
-cp "${script_dir}/${module_name}.service" "/etc/systemd/system/${module_name}.service"
-chmod 777 "/etc/systemd/system/${module_name}.service"
+chmod 644 "${install_dir}/${module_name}/${module_name}.service"
+ln -s "/etc/systemd/system/${module_name}.service" "${install_dir}/${module_name}/${module_name}.service"
 systemctl enable "${module_name}.service"
 systemctl start "${module_name}.service"
