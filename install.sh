ea8fed476b0487eaa58a58f07c4e0009fd133c69
nulysses <adr.zivil@gmail.com>
2021-05-01 12:21:41 +0200

    1

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 if [[ "$(whoami)" != "root" ]]; then
   read -p "[ERROR] must be root!"
@@ -21,6 +22,6 @@ chmod +x "${install_dir}/${module_name}/"* -R
 
 chmod 644 "${install_dir}/${module_name}/${module_name}.service"
 rm -rf "/etc/systemd/system/${module_name}.service"
-ln -s "/etc/systemd/system/${module_name}.service" "${install_dir}/${module_name}/${module_name}.service"
+ln -s "${install_dir}/${module_name}/${module_name}.service" "/etc/systemd/system/${module_name}.service"
 systemctl enable "${module_name}.service"
 systemctl start "${module_name}.service"
